142216421cdb1b2a4a482da4d0f0eb04077fa618
NICOLA NSENGIYUMVA <fatepepe66@gmail.com>
2025-12-23 14:37:13 +0200

    add style to display element

--- app.py
+++ app.py
@@ -3,11 +3,12 @@ from flask import Flask, render_template , request
 import pickle
 
 app = Flask(__name__)
-cv = pickle.load(open("models/cv.pkl"))
-clf = pickle.load(open("models/clf.pkl"))
+cv = pickle.load(open("models/cv.pkl", "rb"))
+clf = pickle.load(open("models/clf.pkl", "rb"))
 
 
-.route("/")
+
+@app    .route("/")
 def home():
     return render_template("index.html")
 
